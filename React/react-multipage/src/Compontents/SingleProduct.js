3e464bed27cc052883b02e65093f4137444efb4e
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2023-06-20 13:48:36 +0530

    dynamic-route

--- React/react-multipage/src/Compontents/SingleProduct.js
+++ React/react-multipage/src/Compontents/SingleProduct.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 const SingleProduct = () => {
+  //   console.log(useParams());
+  const { productID } = useParams();
+  const [product, setProduct] = useState("");
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${productID}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  console.log(product);
+
   return (
     <div>
       <h2>Single Product</h2>
